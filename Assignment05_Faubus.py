55d679e7900ce24a71e2dc142086807fe437166d
julia.faubus <julia.faubus@gmail.com>
2024-03-03 22:24:46 -0800

    Added the greet me method in the BasicMathOperations class

---
+++ Assignment05_Faubus.py
@@ -0,0 +1,9 @@
+#starts the BasicMath Operations class
+class BasicMathOperations():
+    #initializer
+    def __init__(self):
+        None
+
+    #method for greeting someone with their firt and last name
+    def Greet(self, first, last):
+        return f"hello, {first} {last}"
