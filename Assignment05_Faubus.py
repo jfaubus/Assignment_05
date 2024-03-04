dfe4920197213a3fb5261cbfd42428d5cb15debe
julia.faubus <julia.faubus@gmail.com>
2024-03-03 22:26:30 -0800

    Added the adding numbers method

--- Assignment05_Faubus.py
+++ Assignment05_Faubus.py
@@ -7,3 +7,7 @@ class BasicMathOperations():
     #method for greeting someone with their firt and last name
     def Greet(self, first, last):
         return f"hello, {first} {last}"
+    
+    #method to return the sum of two numbers
+    def AddNumbers(self, num1, num2):
+        return int(num1) + int(num2)
