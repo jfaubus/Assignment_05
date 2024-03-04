c90c08c97a08b4324add72189c18b1aac6226c33
julia.faubus <julia.faubus@gmail.com>
2024-03-03 22:27:43 -0800

    Added the operations method (with a way to ensure the user's input is valid

--- Assignment05_Faubus.py
+++ Assignment05_Faubus.py
@@ -11,3 +11,18 @@ class BasicMathOperations():
     #method to return the sum of two numbers
     def AddNumbers(self, num1, num2):
         return int(num1) + int(num2)
+    
+    #method to perform the operation of the user's choosing
+    def Operations(self, num1, num2, operation):
+        if operation == "multiply":
+            return num1 * num2
+        elif operation == "divide":
+            return num1 / num2
+        elif operation == "sum":
+            return num1 + num2
+        elif operation == "subtract":
+            return num1 - num2
+        else:
+            print( "the operation was not valid")
+            operation = input("Please enter either multiply, divide, sum, or subtract " )
+            return BasicMathOperations.Operations(self, num1, num2, operation)
